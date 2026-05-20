a37e904a093446cea4cb31b5ce88cdad55fe4ae8
sreeja <sreeja_nampoothiri@comcast.com>
2026-05-20 12:42:57 +0530

    COVERITY TEST. DO NOT MERGE

--- source/TR-181/middle_layer_src/cosa_webconfig_api.c
+++ source/TR-181/middle_layer_src/cosa_webconfig_api.c
@@ -160,7 +160,7 @@ uint32_t getBlobVersion (char *subdoc)
     char buf[64 + 8];
     char subdoc_ver[12];
 
-    snprintf(buf, sizeof(buf), "%s_version", subdoc);
+    snprintf(buf, sizeof(buf), "%s_version");
 
     if (syscfg_get(NULL, buf, subdoc_ver, sizeof(subdoc_ver)) == 0)
     {
