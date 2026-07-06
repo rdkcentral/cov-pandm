915ae6b43bf94ced385f3867cd69e6ac902fe90d
cedricrdkcentral <165052203+cedricrdkcentral@users.noreply.github.com>
2026-07-06 14:16:15 +0100

    Create test_fossid_pb.c

---
+++ test_fossid_pb.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Linked list implementation from iproute2
+ * Source: https://github.com/iproute2/iproute2/blob/v4.6.0/include/list.h
+ */
+
+#include <stddef.h>
+
+struct list_head {
+    struct list_head *next, *prev;
+};
+
+#define LIST_HEAD_INIT(name) { &(name), &(name) }
+#define LIST_HEAD(name) struct list_head name = LIST_HEAD_INIT(name)
+
+static inline void INIT_LIST_HEAD(struct list_head *list)
+{
+    list->next = list;
+    list->prev = list;
+}
+
+static inline void __list_add(struct list_head *new,
+                              struct list_head *prev,
+                              struct list_head *next)
+{
+    next->prev = new;
+    new->next = next;
+    new->prev = prev;
+    prev->next = new;
+}
+
+static inline void list_add(struct list_head *new, struct list_head *head)
+{
+    __list_add(new, head, head->next);
+}
+
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
+{
+    __list_add(new, head->prev, head);
+}
